f4109583f33c10e9e44dbf2c30633a7aa0a38a4e
Jason <jasonygodfrey@gmail.com>
2023-12-21 05:19:00 -0800

    davinci

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -1,4 +1,3 @@
-// pages/api/chat.js
 export default async function handler(req, res) {
     if (req.method === 'POST') {
         let userMessage = req.body.message;
@@ -7,27 +6,29 @@ export default async function handler(req, res) {
         if (!userMessage || userMessage.trim() === '') {
             userMessage = "Ask an oncology question";
         }
+
         try {
+            // Structuring the prompt for a conversational context
+            const prompt = `User: ${userMessage}\nBot:`;
             const response = await fetch('https://api.openai.com/v1/engines/davinci/completions', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
-                    'Authorization': `Bearer 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`
+                    'Authorization': `Bearer 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` // Ensure to use secure handling for API keys
                 },
                 body: JSON.stringify({
-                    prompt: userMessage,
-                    max_tokens: 150 // or another number of tokens you prefer
-                    // You can include other parameters as needed
+                    prompt: prompt,
+                    max_tokens: 150,
+                    temperature: 0.7 // Adjust as needed for the desired level of creativity
                 })
             });
 
             const data = await response.json();
-        // Check if userMessage is empty, if so, set a default message
-        console.log(data);
+            console.log(data); // Log the response for debugging
 
             if (data.choices && data.choices.length > 0) {
                 // Send back the AI's response
-                res.status(200).json({ reply: data.choices[0].text });
+                res.status(200).json({ reply: data.choices[0].text.trim() });
             } else {
                 // Handle no choices in response
                 res.status(200).json({ reply: "I'm sorry, I couldn't generate a response." });
