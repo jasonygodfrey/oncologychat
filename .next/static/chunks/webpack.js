736dac51a84166abc1d926e3d0aa542cccb5742e
Jason <jasonygodfrey@gmail.com>
2023-12-21 08:27:24 -0800

    clear chatbox styling

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0ab7ad3d55a33ac8"; }
+/******/ 		__webpack_require__.h = function() { return "cfe07fc7bc50eb27"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
